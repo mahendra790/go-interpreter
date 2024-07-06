4421248c55873a3afdd36d810393b1281acf54f1
Mahendra Suthar <mahendrasuthar@sharechat.co>
2024-07-06 20:17:51 +0530

    use repl

--- main.go
+++ main.go
@@ -1,31 +1,10 @@
 package main
 
 import (
-	"bytes"
-	"fmt"
-	"monkey/src/evaluator"
-	"monkey/src/lexer"
-	"monkey/src/object"
-	"monkey/src/parser"
+	"monkey/src/repl"
 	"os"
 )
 
 func main() {
-	content, err := os.ReadFile("p.mky")
-	if err != nil {
-		fmt.Printf("error")
-		return
-	}
-
-	str := string(content)
-
-	l := lexer.New(str)
-	p := parser.New(l)
-
-	program := p.ParseProgram()
-
-	env := object.NewEnvironment()
-	evaluated := evaluator.Eval(program, env, &bytes.Buffer{})
-
-	fmt.Println(evaluated.Inspect())
+	repl.Start(os.Stdin, os.Stdout)
 }
